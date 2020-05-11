476cf43b928aaaebd48893a5e3d84e8981525c75
Diego Hernando Revenga <diego.hereven@gmail.com>
2020-05-11 21:32:24 +0200

    Created app skeleton with spring initializr

---
+++ src/main/java/com/diego/hernando/orchestTest/OrchestTestApplication.java
@@ -0,0 +1,13 @@
+package com.diego.hernando.orchestTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/hernando/orchestTest/OrchestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.hernando.orchestTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
