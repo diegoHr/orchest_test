f8dc20dfbf9a8319f870eba9541974c6beeb0fcd
Diego Hernando Revenga <diego.hereven@gmail.com>
2020-05-12 14:19:22 +0200

    Improved jackson parse of WorkSignType and WorkSignRecordType

--- src/main/java/com/diego/hernando/orchestTest/model/WorkSignType.java
+++ src/main/java/com/diego/hernando/orchestTest/model/WorkSignType.java
@@ -1,5 +1,8 @@
 package com.diego.hernando.orchestTest.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum WorkSignType {
     WORK,REST
 }
