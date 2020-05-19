204f54597df7367283f5e82db95fc6df8c83020e
Diego Hernando Revenga <diego.hereven@gmail.com>
2020-05-19 22:23:29 +0200

    Added Swagger

--- src/main/java/com/diego/hernando/orchestTest/OrchestTestApplication.java
+++ src/main/java/com/diego/hernando/orchestTest/OrchestTestApplication.java
@@ -1,15 +1,20 @@
 package com.diego.hernando.orchestTest;
 
+import com.diego.hernando.orchestTest.configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class OrchestTestApplication {
+@Import(SwaggerConfiguration.class)
+public class OrchestTestApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrchestTestApplication.class, args);
@@ -20,6 +25,14 @@ public class OrchestTestApplication {
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.forLanguageTag("es_ES"));
 		return localeResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
 	}
 
 
